692de5b601db55aa8b4704ddeff0b02b8db8d4c0
Brandocean <brandocean2016@gmail.com>
2024-01-31 15:13:28 -0600

    Agregar cors

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const cors = require('cors')
 const routerApi = require('./routes') //* El index.js lo busca por defecto
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com']
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hola este es mi server en Express')
